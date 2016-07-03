87c152542f76d95187b3bc7fe1d9bbe04150e12c
Everfighting <cbb903601682@163.com>
2016-07-03 16:22:00 +0800

    update

--- 032.py
+++ 032.py
@@ -4,3 +4,18 @@
 a = ['one', 'two', 'three']
 for i in a[::-1]:
 	print i
+
+for m in range(0,10,2):
+	print m
+
+
+# for n in range(0,10,-2):
+# 		print n
+# 错误之处：[::-1]可以完全倒置列表，但是range（0,10，-2）不可以。
+
+for m in range(10,0,-2):
+	print m
+
+
+
+
--- 033.py
+++ 033.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
+# join 和 split方法的使用,join用来连接字符串，split恰好相反，拆分字符串的。
 L = [1,2,3,4,5]
 s1 = ','.join(str(n) for n in L)
 print s1
+
+s2 = ' '.join('12345')
+print s2
+
+s3 = '*'.join(['1','2','3','4','5'])
+print s3
+
+s4 = s3.split('*')
+print s4
+s5 = s2.split(" ")
+print s5
+s6 = s1.split(",", 0)
+print s6
+s7 = s1.split(",", 1)
+print s7
+s8 = s1.split(",", -1)
+print s8
