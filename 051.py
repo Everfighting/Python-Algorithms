c1da4d79758df3e6e963683115f866158e0b6331
Everfighting <cbb903601682@163.com>
2016-07-04 20:22:14 +0800

    update

--- 051.py
+++ 051.py
@@ -2,9 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 if __name__ == '__main__':
-    a = 077
-    b = a & 3
+    a = 2
+    b = a & 7
     print 'a & b = %d' % b
-    b &= 7
+    b &= 2
     print 'a & b = %d' % b
+
+# 按照二进制进行位运算！
 
