0d2622d8056f11a3604d7de2d12d4335fb61486c
cbb <903601682@qq.com>
2018-02-08 11:59:08 +0800

    增加题目描述

--- 003.py
+++ 003.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
+"""
+一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+"""
 import math
+# 根据描述可以判断该数不会超过10000
 for i in range(10000):
     #math.sqrt开平方
     x = int(math.sqrt(i + 100))
