0e5f70ced6974fc4059c0eeda772c6f4f5c12938
cbb <903601682@qq.com>
2018-02-11 16:49:24 +0800

    获取该年经过的天数

--- 004.py
+++ 004.py
@@ -1,25 +1,16 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+def get_days(year, month, day):
+    months1 = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366] # 闰年
+    months2 = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365] # 平年
+    if ((year%4==0) and (year%100!=0)) or ((year%100==0) and (year%400==0)):
+        days = months1[month-1] + day
+    else:
+        days = months2[month-1] + day
+    print("该日是该年的{}天".format(days))
 
-year = int(raw_input('year:\n'))
-month = int(raw_input('month:\n'))
-day = int(raw_input('day:\n'))
-
-#将之前月份和当月天数，分开计算，减少不必要的if...else的判断。
-months = (0,31,59,90,120,151,181,212,243,273,304,334)
-if 0 < month <= 12:
-    sum = months[month - 1]
-else:
-    print 'data error'
-sum += day
-#使用leap来标记是否是闰年
-leap = 0
-if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
-    leap = 1
-#对是闰年且月份大约2月的天数增加一天
-if (leap == 1) and (month > 2):
-    sum += 1
-print 'it is the %dth day.' % sum
-
-#与002题思路较相似，利用数组来表示每个月对应的天数，通过月份与当月天数的分离，使得计算更为清晰。
-#判断闰年中使用了leap作为标志符，若为闰年则设置leap为1，给闰年月份大于2月的增加一天。
+if __name__ == '__main__':
+    year = int(input("年：\n"))
+    month = int(input("月:\n"))
+    day = int(input("日:\n"))
+    get_days(year, month, day)
