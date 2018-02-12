5ad8e9f4c7fd03e3b5e5f1c12e54f2dea34ba98d
cbb <903601682@qq.com>
2018-02-12 11:38:37 +0800

    添加题目描述

--- 005.py
+++ 005.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
+"""
+题目:输入三个整数x,y,z,请把这三个数由小到大输出。
+"""
+i# 列表的sort方法
 l = []
 for i in range(3):
     x = int(raw_input('integer:\n'))
     l.append(x)
 l.sort()
-print l
+print(l)
