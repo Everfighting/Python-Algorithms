d05c948a9bc3c31a28310c2a5d4318d042991fc5
cbb <903601682@qq.com>
2018-02-24 19:32:38 +0800

    修改dict方法

--- 009.py
+++ 009.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 import time
 myD = {1: 'a', 2: 'b'}
-for key, value in dict.items(myD):
+for key, value in myD.items():
     print key, value
     time.sleep(1) # 暂停 1 秒
 # dict.items()、dict.value()、dict.key()
