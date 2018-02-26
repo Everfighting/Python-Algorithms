4c258fa0b5055d9cad5645ce3c252d0719e558a1
cbb <903601682@qq.com>
2018-02-26 20:21:01 +0800

    strftime用法

--- 010.py
+++ 010.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import time
 
-print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
 # 暂停一秒
 time.sleep(1)
 
-print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+# time.strftime将时间转成需要的格式
